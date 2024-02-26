6b16e052825c1851126eda7582b162ed07500aa6
Weronika Bednarz <weronikaabednarz@gmail.com>
2024-02-26 12:18:12 +0100

    Creating an initial division of window space

--- excel_app.py
+++ excel_app.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 
 window = tk.Tk()
+window.title("Excel Application")
 
 style = ttk.Style(window)       # enable to apply a theme to app
 window.tk.call("source", "forest-light.tcl")
@@ -9,6 +10,10 @@ window.tk.call("source", "forest-dark.tcl")
 style.theme_use("forest-dark")
 #style.theme_use("forest-light")
 
+frame = ttk.Frame(window)
+frame.pack()
 
+widgets_frame = ttk.LabelFrame(frame, text = "Insert Row")
+widgets_frame.grid(row = 0, column = 0)
 
 window.mainloop()
