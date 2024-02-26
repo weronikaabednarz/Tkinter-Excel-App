5a8c0bf59ab710b026e3a3006e8565048798b3ee
Weronika Bednarz <weronikaabednarz@gmail.com>
2024-02-26 15:22:36 +0100

    Loading data from Excel into the treeview

--- excel_app.py
+++ excel_app.py
@@ -1,5 +1,18 @@
 import tkinter as tk
 from tkinter import ttk
+import openpyxl
+
+def load_data():
+    path = "C:\\Users\\weron\\Desktop\\projekty\\Tkinter Excel App\\people.xlsx"
+    workbook = openpyxl.load_workbook(path)
+    sheet = workbook.active
+
+    list_values = list(sheet.values)
+    print(list_values)
+    for col_name in list_values[0]:
+        treeview.heading(col_name, text = col_name)
+    for value_tuple in list_values[1:]:
+        treeview.insert('', tk.END, values = value_tuple)
 
 def toggle_mode():
     if mode_switch.instate(["selected"]):       # selected - press on it once
@@ -33,7 +46,7 @@ age_spinbox.grid(row = 1, column = 0, padx = 5, pady = 5, sticky = "ew")
 
 interests_combobox = ttk.Combobox(widgets_frame, values = ["Programming", "Management", "Marketing",
                                                            "Data analysis", "Computer graphics", "Web design",
-                                                           "Accounting", "International trade""Employment law"])
+                                                           "Accounting", "International trade", "Employment law"])
 interests_combobox.current(0)
 interests_combobox.grid(row = 2, column = 0, padx = 5, pady = 5, sticky = "ew")
 
@@ -52,19 +65,19 @@ mode_switch.grid(row = 6, column = 0, padx = 5, pady = 10, sticky = "ew")
 
 treeFrame = ttk.Frame(frame)
 treeFrame.grid(row = 0, column = 1, pady = 10)
-
+#----------------tree scroll----------------------------------------------------------------------------------------------------------------
 treeScroll = ttk.Scrollbar(treeFrame)   # scroll button, right side, full axis y
 treeScroll.pack(side = "right", fill = "y")
 
 cols = ("Name", "Age", "Interests", "Employment")
 treeview = ttk.Treeview(treeFrame, show = "headings",
-                        yscrollcommand = treeScroll.set,
-                        columns = cols, height = 11)
-treeview.column("Name", width = 100)
+                        yscrollcommand = treeScroll.set, columns = cols, height = 11)
+treeview.column("Name", width = 140)
 treeview.column("Age", width = 50)
-treeview.column("Interests", width = 100)
+treeview.column("Interests", width = 140)
 treeview.column("Employment", width = 100)
 treeview.pack()
 treeScroll.config(command = treeview.yview)
+load_data()
 
 window.mainloop()
