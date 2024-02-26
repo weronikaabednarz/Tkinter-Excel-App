894e6de9f9c760b407bdb2c1659a89f05b18edab
Weronika Bednarz <weronikaabednarz@gmail.com>
2024-02-26 13:07:59 +0100

    Created: spinbox, combobox and checkbutton

--- excel_app.py
+++ excel_app.py
@@ -16,4 +16,26 @@ frame.pack()
 widgets_frame = ttk.LabelFrame(frame, text = "Insert Row")
 widgets_frame.grid(row = 0, column = 0)
 
+name_entry = ttk.Entry(widgets_frame)
+name_entry.insert(0, "Name")
+name_entry.bind("<FocusIn>", lambda e: name_entry.delete('0', 'end'))    # focus in - press on entry
+name_entry.grid(row = 0, column = 0, sticky = "ew")
+
+age_spinbox = ttk.Spinbox(widgets_frame, from_ = 18, to = 100)
+age_spinbox.insert(0, "Age")
+age_spinbox.bind("<FocusIn>", lambda e: age_spinbox.delete('0', 'end'))
+age_spinbox.grid(row = 1, column = 0, sticky = "ew")
+
+interests_combobox = ttk.Combobox(widgets_frame, values = ["Programming", "Management", "Marketing",
+                                                           "Data analysis", "Computer graphics", "Web design",
+                                                           "Accounting", "International trade""Employment law"])
+interests_combobox.current(0)
+interests_combobox.grid(row = 2, column = 0, sticky = "ew")
+
+a = tk.BooleanVar()
+employed_checkbutton = ttk.Checkbutton(widgets_frame, text = "Employed", variable = a)
+employed_checkbutton.grid(row = 3, column = 0, sticky = "w")
+
+
+
 window.mainloop()
