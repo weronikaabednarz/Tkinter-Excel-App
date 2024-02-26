0b108d10dfb85f073f6f33198939caa35eb05c10
Weronika Bednarz <weronikaabednarz@gmail.com>
2024-02-26 13:18:45 +0100

    Modified application view

--- excel_app.py
+++ excel_app.py
@@ -14,28 +14,29 @@ frame = ttk.Frame(window)
 frame.pack()
 
 widgets_frame = ttk.LabelFrame(frame, text = "Insert Row")
-widgets_frame.grid(row = 0, column = 0)
+widgets_frame.grid(row = 0, column = 0, padx = 20, pady = 10)
 
 name_entry = ttk.Entry(widgets_frame)
 name_entry.insert(0, "Name")
 name_entry.bind("<FocusIn>", lambda e: name_entry.delete('0', 'end'))    # focus in - press on entry
-name_entry.grid(row = 0, column = 0, sticky = "ew")
+name_entry.grid(row = 0, column = 0, padx = 5, pady = (0,5), sticky = "ew")     # distance of 0 pixels from the top edge and 5 pixels from the bottom edge
 
 age_spinbox = ttk.Spinbox(widgets_frame, from_ = 18, to = 100)
 age_spinbox.insert(0, "Age")
 age_spinbox.bind("<FocusIn>", lambda e: age_spinbox.delete('0', 'end'))
-age_spinbox.grid(row = 1, column = 0, sticky = "ew")
+age_spinbox.grid(row = 1, column = 0, padx = 5, pady = 5, sticky = "ew")
 
 interests_combobox = ttk.Combobox(widgets_frame, values = ["Programming", "Management", "Marketing",
                                                            "Data analysis", "Computer graphics", "Web design",
                                                            "Accounting", "International trade""Employment law"])
 interests_combobox.current(0)
-interests_combobox.grid(row = 2, column = 0, sticky = "ew")
+interests_combobox.grid(row = 2, column = 0, padx = 5, pady = 5, sticky = "ew")
 
 a = tk.BooleanVar()
 employed_checkbutton = ttk.Checkbutton(widgets_frame, text = "Employed", variable = a)
-employed_checkbutton.grid(row = 3, column = 0, sticky = "w")
+employed_checkbutton.grid(row = 3, column = 0, padx = 5, pady = 5, sticky = "w")
 
-
+button = ttk.Button(widgets_frame, text = "Insert")
+button.grid(row = 4, column = 0, sticky = "news")
 
 window.mainloop()
